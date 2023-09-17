4dc525ab2f30c436e1006d4ea21bd77f95094ab3
Alek <92643775+barealek@users.noreply.github.com>
2023-09-17 22:52:46 +0200

    Move minilink to mini

--- miniLink/internal/models/mini.go
+++ miniLink/internal/models/mini.go
@@ -6,5 +6,4 @@ type Mini struct {
 	MiniLink string `json:"minilink" bson:"minilink"` // The path of the minilink
 	Clicks   uint64 `json:"clicks" bson:"clicks"`     // Amount of clicks
 	Owner    string `json:"owner" bson:"owner"`       // The owner of the minilink
-	Random   bool   `json:"random" bson:"random"`     // If a custom minilink is set or not
 }
