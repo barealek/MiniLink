a293de45fb8a127ce0d78ab304e49a9be3f1d586
Alek <92643775+barealek@users.noreply.github.com>
2023-09-17 17:36:17 +0200

    Finish the app entrypoint w/ database and fiber

--- miniLink/cmd/main.go
+++ miniLink/cmd/main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
+	"github.com/barealek/minilink/internal/database"
 	"github.com/barealek/minilink/pkg/shutdown"
+
+	gojson "github.com/goccy/go-json"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
@@ -30,4 +35,39 @@ func main() {
 	shutdown.Shutdown()
 }
 
-func run() (func(), error) {}
+func run() (func(), error) {
+	app, cleanup, err := buildMiniLink()
+	if err != nil {
+		return nil, err
+	}
+
+	// Run the app in a goroutine
+	go func() {
+		app.Listen(":3000")
+	}()
+
+	return func() {
+		app.Shutdown()
+		cleanup()
+	}, nil
+}
+
+func buildMiniLink() (*fiber.App, func(), error) {
+
+	// Connect to the MongoDB database
+	db, err := database.BootstrapMongo(os.Getenv("MONGO_CONN_URI"), "miniLink", 10*time.Second)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	app := fiber.New(fiber.Config{
+		JSONEncoder: gojson.Marshal,
+		JSONDecoder: gojson.Unmarshal,
+	})
+
+	// controllers.RegisterGroups(app)
+
+	return app, func() {
+		database.CloseMongo(db)
+	}, nil
+}
