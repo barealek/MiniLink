4d5ace37b599cd435b5dc9422c70a478f78fa8af
Alek <92643775+barealek@users.noreply.github.com>
2023-09-18 22:13:59 +0200

    Update controllers.go

--- miniLink/internal/controllers/controllers.go
+++ miniLink/internal/controllers/controllers.go
@@ -8,7 +8,10 @@ import (
 )
 
 func RegisterGroups(app *fiber.App) {
-	api := app.Group("/api")
+	api := app.Group("/a")
+	// Since i have a reverse proxy in front of this and also have to serve a front end, I'm using the /a prefix for the api.
+	// This is not necessary if you are not using a reverse proxy and are not serving a front end. You can just use app.Group("/").
+
 	root.RegisterControllers(api)
 	minis.RegisterControllers(api.Group("/mini"))
 }
