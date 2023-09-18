519d031561ec1e4f6fd386424aa933299ddae80a
Alek <92643775+barealek@users.noreply.github.com>
2023-09-18 21:06:09 +0200

    Update the Mini type

--- miniLink/internal/models/mini.go
+++ miniLink/internal/models/mini.go
@@ -1,9 +1,10 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Mini struct {
-	ID       uint64 `json:"id" bson:"_id,omitempty"`  // ID of the miniLink
-	Redirect string `json:"link" bson:"link"`         // Link to redirect to
-	MiniLink string `json:"minilink" bson:"minilink"` // The path of the minilink
-	Clicks   uint64 `json:"clicks" bson:"clicks"`     // Amount of clicks
-	Owner    string `json:"owner" bson:"owner"`       // The owner of the minilink
+	ID       primitive.ObjectID `json:"id" bson:"_id,omitempty"`  // ID of the miniLink
+	Redirect string             `json:"link" bson:"link"`         // Link to redirect to
+	MiniLink string             `json:"minilink" bson:"minilink"` // The path of the minilink
+	Clicks   uint64             `json:"clicks" bson:"clicks"`     // Amount of clicks
 }
